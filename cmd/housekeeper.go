2b4790795777d2920f71af1dd49891e9b409e876
Chris Hager <chris@linuxuser.at>
2022-09-09 21:09:49 +0200

    housekeeper doesn't need database access (#114)

--- cmd/housekeeper.go
+++ cmd/housekeeper.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/flashbots/mev-boost-relay/beaconclient"
 	"github.com/flashbots/mev-boost-relay/common"
-	"github.com/flashbots/mev-boost-relay/database"
 	"github.com/flashbots/mev-boost-relay/datastore"
 	"github.com/flashbots/mev-boost-relay/services/housekeeper"
 	"github.com/sirupsen/logrus"
@@ -19,7 +18,6 @@ func init() {
 
 	housekeeperCmd.Flags().StringSliceVar(&beaconNodeURIs, "beacon-uris", defaultBeaconURIs, "beacon endpoints")
 	housekeeperCmd.Flags().StringVar(&redisURI, "redis-uri", defaultRedisURI, "redis uri")
-	housekeeperCmd.Flags().StringVar(&postgresDSN, "db", defaultPostgresDSN, "PostgreSQL DSN")
 
 	housekeeperCmd.Flags().StringVar(&network, "network", defaultNetwork, "Which network to use")
 }
@@ -57,21 +55,9 @@ var housekeeperCmd = &cobra.Command{
 			log.WithError(err).Fatalf("Failed to connect to Redis at %s", redisURI)
 		}
 
-		log.Infof("Connecting to Postgres database...")
-		db, err := database.NewDatabaseService(postgresDSN)
-		if err != nil {
-			log.WithError(err).Fatalf("Failed to connect to Postgres database at %s", postgresDSN)
-		}
-
-		ds, err := datastore.NewDatastore(log, redis, db)
-		if err != nil {
-			log.WithError(err).Fatalf("Failed to create datastore")
-		}
-
 		opts := &housekeeper.HousekeeperOpts{
 			Log:          log,
 			Redis:        redis,
-			Datastore:    ds,
 			BeaconClient: beaconClient,
 		}
 		service := housekeeper.NewHousekeeper(opts)
