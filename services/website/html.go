f258d089a13b75967e45b96f672a2e79527591eb
s bacha <sam@manifoldfinance.com>
2022-09-16 21:29:39 -0700

    feat(website): add agpl footer notice

--- services/website/html.go
+++ services/website/html.go
@@ -191,8 +191,39 @@ func parseIndexTemplate() (*template.Template, error) {
             </center>
         </div>
     </div>
-</body>
+<footer>
+        <div>
+          <span>
+            <a target="_top" href="https://flashbots.net">
+           <p>ⓒ 2022 - Flashbots & the Flashbots contributors</p>  
+            </a>
+          </span>
+          <span>
+              <a href="https://github.com/flashbots/mev-boost-relay">This program is free software: github.com/flashbots/mev-boost-relay</a>
+          </span>
+        </div>
+        </footer>
+    
+    </body>
+<!--
+	SPDX-License-Identifier: AGPL
+	SPDXVersion: SPDX-2.2
+	MEV-Boost-Relay
+	Copyright (C) 2022 Flashbots
 
-</html>
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
+    </html>
 `)
 }
