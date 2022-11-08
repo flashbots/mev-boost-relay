5d8ed25838dadd9cd4e91b3194bdb898e1892bc9
Chris Hager <chris@linuxuser.at>
2022-11-08 10:11:32 +0100

    minor cleanup

--- database/vars/tables.go
+++ database/vars/tables.go
@@ -6,10 +6,10 @@ import "github.com/flashbots/mev-boost-relay/common"
 var (
 	tableBase = common.GetEnv("DB_TABLE_PREFIX", "dev")
 
+	TableMigrations             = tableBase + "_migrations"
 	TableValidatorRegistration  = tableBase + "_validator_registration"
 	TableExecutionPayload       = tableBase + "_execution_payload"
 	TableBuilderBlockSubmission = tableBase + "_builder_block_submission"
 	TableDeliveredPayload       = tableBase + "_payload_delivered"
 	TableBlockBuilder           = tableBase + "_blockbuilder"
-	TableMigrations             = tableBase + "_migrations"
 )
