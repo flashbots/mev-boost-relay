507dd50e3bc502d7d69a946791f572954515346e
Chris Hager <chris@linuxuser.at>
2022-11-08 19:45:15 +0100

    make concurrent index creation work, add db tests

--- database/migrations/002_bid_remove_isbest_add_receivedat.go
+++ database/migrations/002_bid_remove_isbest_add_receivedat.go
@@ -9,12 +9,13 @@ var Migration002RemoveIsBestAddReceivedAt = &migrate.Migration{
 	Id: "002-remove-isbest-add-receivedat",
 	Up: []string{`
 		ALTER TABLE ` + vars.TableBuilderBlockSubmission + ` ADD received_at timestamp;
-		CREATE INDEX CONCURRENTLY IF NOT EXISTS ` + vars.TableBuilderBlockSubmission + `_received_idx ON ` + vars.TableBuilderBlockSubmission + `(received_at DESC);
 
 		ALTER TABLE ` + vars.TableBuilderBlockSubmission + ` DROP COLUMN was_most_profitable;
 		DROP INDEX IF EXISTS ` + vars.TableBuilderBlockSubmission + `_mostprofit_idx;
 
 		ALTER TABLE ` + vars.TableBlockBuilder + ` DROP COLUMN num_submissions_topbid;
+	`, `
+		CREATE INDEX CONCURRENTLY IF NOT EXISTS ` + vars.TableBuilderBlockSubmission + `_received_idx ON ` + vars.TableBuilderBlockSubmission + `(received_at DESC);
 	`},
 	Down: []string{},
 
