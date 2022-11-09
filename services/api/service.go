6f443fcc901d7925d8008dab29d0406a00dec522
Chris Hager <chris@linuxuser.at>
2022-11-09 11:24:29 +0100

    data API getBuilderBids requires either slot, block_hash, block_number or builder_pubkey

--- services/api/service.go
+++ services/api/service.go
@@ -1323,19 +1323,8 @@ func (api *RelayAPI) handleDataBuilderBidsReceived(w http.ResponseWriter, req *h
 	}
 
 	if args.Get("cursor") != "" {
-		api.RespondError(w, http.StatusBadRequest, "cursor argument not supported on this API")
+		api.RespondError(w, http.StatusBadRequest, "cursor argument not supported on the getReceivedBids API")
 		return
-	}
-
-	if args.Get("slot") != "" && args.Get("cursor") != "" {
-		api.RespondError(w, http.StatusBadRequest, "cannot specify both slot and cursor")
-		return
-	} else if args.Get("slot") != "" {
-		filters.Slot, err = strconv.ParseUint(args.Get("slot"), 10, 64)
-		if err != nil {
-			api.RespondError(w, http.StatusBadRequest, "invalid slot argument")
-			return
-		}
 	}
 
 	if args.Get("block_hash") != "" {
@@ -1362,6 +1351,18 @@ func (api *RelayAPI) handleDataBuilderBidsReceived(w http.ResponseWriter, req *h
 			return
 		}
 		filters.BuilderPubkey = args.Get("builder_pubkey")
+	}
+
+	// if no other arguments are provided, then slot is mandatory
+	if filters.BlockHash == "" && filters.BlockNumber == 0 && filters.BuilderPubkey == "" && args.Get("slot") == "" {
+		api.RespondError(w, http.StatusBadRequest, "need to query for specific slot or block_hash or block_number or builder_pubkey")
+		return
+	}
+
+	filters.Slot, err = strconv.ParseUint(args.Get("slot"), 10, 64)
+	if err != nil {
+		api.RespondError(w, http.StatusBadRequest, "invalid slot argument")
+		return
 	}
 
 	if args.Get("limit") != "" {
