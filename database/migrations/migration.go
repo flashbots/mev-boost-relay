0bad5f7ba15bec67ba4cea9ba12b359bdc443741
Michael Neuder <mikeneuder@google.com>
2023-02-14 07:30:20 -0500

    unsquash migrations

--- database/migrations/migration.go
+++ database/migrations/migration.go
@@ -10,5 +10,7 @@ var Migrations = migrate.MemoryMigrationSource{
 		Migration001InitDatabase,
 		Migration002RemoveIsBestAddReceivedAt,
 		Migration003Optimistic,
+		Migration004Temp,
+		Migration005Profiling,
 	},
 }
