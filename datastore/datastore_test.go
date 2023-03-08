807901749998ca4f8d6b400a6a3daf1f54592a20
Wasif Iqbal <Wazzymandias@users.noreply.github.com>
2023-03-08 00:16:22 -0600

    Update README to use goerli, fix method parameters for test

--- datastore/datastore_test.go
+++ datastore/datastore_test.go
@@ -21,6 +21,7 @@ func setupTestDatastore(t *testing.T) *Datastore {
 	redisDs, err := NewRedisCache(redisTestServer.Addr(), "")
 	require.NoError(t, err)
 
+	// TODO: create mock memcached instead of passing in nil
 	ds, err := NewDatastore(common.TestLog, redisDs, nil, database.MockDB{})
 	require.NoError(t, err)
 
