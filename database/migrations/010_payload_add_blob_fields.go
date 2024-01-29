0f7505d6248a16734336bb875f8ad7863d0642ea
shana <avalonche@protonmail.com>
2024-01-30 05:08:27 +1100

    add default

--- database/migrations/010_payload_add_blob_fields.go
+++ database/migrations/010_payload_add_blob_fields.go
@@ -10,9 +10,9 @@ import (
 var Migration010PayloadAddBlobFields = &migrate.Migration{
 	Id: "010-payload-add-blob-fields",
 	Up: []string{`
-		ALTER TABLE ` + vars.TableDeliveredPayload + ` ADD blob_gas_used bigint;
-		ALTER TABLE ` + vars.TableDeliveredPayload + ` ADD excess_blob_gas bigint;
-		ALTER TABLE ` + vars.TableDeliveredPayload + ` ADD num_blobs int;
+		ALTER TABLE ` + vars.TableDeliveredPayload + ` ADD blob_gas_used bigint NOT NULL DEFAULT 0;
+		ALTER TABLE ` + vars.TableDeliveredPayload + ` ADD excess_blob_gas bigint NOT NULL DEFAULT 0;
+		ALTER TABLE ` + vars.TableDeliveredPayload + ` ADD num_blobs int NOT NULL DEFAULT 0;
 	`},
 	Down: []string{},
 
