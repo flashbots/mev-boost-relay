44adf4cb0d5569bd89f13ac8d89a3bfc3c3a63c9
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2024-09-24 10:47:30 -0400

    feat: adds opted in status for validators

--- database/migrations/012_add_mev_commit_column_builders.go
+++ database/migrations/012_add_mev_commit_column_builders.go
@@ -8,10 +8,10 @@ import (
 var Migration012AddMevCommitColumnBuilders = &migrate.Migration{
 	Id: "012-add-mev-commit-column-builders",
 	Up: []string{`
-		ALTER TABLE ` + vars.TableBlockBuilder + ` ADD is_mev_commit_enabled boolean NOT NULL DEFAULT false;
+		ALTER TABLE ` + vars.TableBlockBuilder + ` ADD is_mev_commit_opted_in boolean NOT NULL DEFAULT false;
 	`},
 	Down: []string{`
-		ALTER TABLE ` + vars.TableBlockBuilder + ` DROP COLUMN is_mev_commit_enabled;
+		ALTER TABLE ` + vars.TableBlockBuilder + ` DROP COLUMN is_mev_commit_opted_in;
 	`},
 
 	DisableTransactionUp:   true,
