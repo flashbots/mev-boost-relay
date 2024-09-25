f95c08659e4102a39e22da7c5e4f0db5047b23f5
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2024-09-25 10:26:37 -0400

    feat: adds the new table migration

---
+++ database/migrations/013_add_mev_commit_validators_table.go
@@ -0,0 +1,24 @@
+package migrations
+
+import (
+	"github.com/flashbots/mev-boost-relay/database/vars"
+	migrate "github.com/rubenv/sql-migrate"
+)
+
+var Migration013AddMevCommitValidatorsTable = &migrate.Migration{
+	Id: "013-add-mev-commit-validators-table",
+	Up: []string{`
+		CREATE TABLE IF NOT EXISTS ` + vars.TableMevCommitValidators + ` (
+			id SERIAL PRIMARY KEY,
+			bls_key VARCHAR(98) NOT NULL,
+			is_opted_in BOOLEAN NOT NULL DEFAULT false,
+			timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+		);
+	`},
+	Down: []string{`
+		DROP TABLE mev_commit_validator_registration;
+	`},
+
+	DisableTransactionUp:   true,
+	DisableTransactionDown: true,
+}
