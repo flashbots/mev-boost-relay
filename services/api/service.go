ece5972d0833ef96706cc8d960ec5c91f633dcf4
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2024-10-09 16:11:50 -0400

    nit: reduce log level

--- services/api/service.go
+++ services/api/service.go
@@ -1772,7 +1772,7 @@ func (api *RelayAPI) checkSubmissionSlotDetails(w http.ResponseWriter, log *logr
 			}
 		}
 		duration := time.Since(start)
-		log.WithField("duration", duration).Info("MEV-Commit check completed")
+		log.WithField("duration", duration).Trace("MEV-Commit check completed")
 	}
 
 	// Timestamp check
