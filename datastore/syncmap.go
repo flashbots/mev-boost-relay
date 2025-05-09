d64d1cb3957121f141386928343fb307ea640f28
Ilya Lukyanov <73885545+ilyaluk@users.noreply.github.com>
2025-05-09 12:23:13 +0200

    use typed wrapper around sync.Map (#726)

---
+++ datastore/syncmap.go
@@ -0,0 +1,28 @@
+package datastore
+
+import "sync"
+
+type syncMap[K comparable, V any] struct {
+	m sync.Map
+}
+
+func (m *syncMap[K, V]) Load(key K) (V, bool) { //nolint:ireturn
+	var vZero V
+
+	v, ok := m.m.Load(key)
+	if !ok {
+		return vZero, false
+	}
+
+	vv, ok := v.(V)
+	if !ok {
+		// should be unreachable
+		return vZero, false
+	}
+
+	return vv, true
+}
+
+func (m *syncMap[K, V]) Store(key K, value V) {
+	m.m.Store(key, value)
+}
